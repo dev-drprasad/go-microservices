aa9b70ae8220080a3b65e840fa4c7e2cbcc260c6
REDDY PRASAD <dev.drprasad@aim.com>
2020-10-04 17:22:23 +0530

    🐛 Fix category add breaking app

--- web/src/shared/hooks.js
+++ web/src/shared/hooks.js
@@ -1,47 +1,42 @@
-import {message} from 'antd';
-import {useCallback, useContext, useEffect, useMemo, useState} from 'react';
+import { message } from "antd";
+import { useCallback, useContext, useEffect, useMemo, useState } from "react";
 // import userService from "src/shared/services/user.service";
-import NS from 'shared/utils/NS';
-import {AuthContext} from './contexts';
+import NS from "shared/utils/NS";
+import { AuthContext } from "./contexts";
 
-export const cr = '\n';
-export const tab = '\t';
+export const cr = "\n";
+export const tab = "\t";
 
 function getType(data) {
-  if (data === null) return 'Null';
-  if (data === undefined) return 'Undefined';
-  if (typeof data === 'string') return 'String';
-  if (typeof data === 'number' && !Number.isNaN(data)) return 'Number';
-  if (Number.isNaN(data)) return 'NaN';
-  if (typeof data === 'boolean') return 'Boolean';
-  if (data instanceof Array)
-    return 'Array';  // always should be before `Object`
-  if (data instanceof Object) return 'Object';
+  if (data === null) return "Null";
+  if (data === undefined) return "Undefined";
+  if (typeof data === "string") return "String";
+  if (typeof data === "number" && !Number.isNaN(data)) return "Number";
+  if (Number.isNaN(data)) return "NaN";
+  if (typeof data === "boolean") return "Boolean";
+  if (data instanceof Array) return "Array"; // always should be before `Object`
+  if (data instanceof Object) return "Object";
 
-  return '';
+  return "";
 }
 
 // https://stackoverflow.com/a/2117523
 function uuidv4() {
-  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11)
-      .replace(
-          /[018]/g,
-          (c) =>
-              (c ^
-               (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4))))
-                  .toString(16));
+  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+    (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16)
+  );
 }
 
 const defaultFetchOptions = {
   headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
+    Accept: "application/json",
+    "Content-Type": "application/json",
   },
 };
 
 export function useFetch(url, opts) {
-  const [[rId, fresh], setParams] = useState(() => ['', false]);
-  const [response, setResponse] = useState([undefined, new NS('INIT')]);
+  const [[rId, fresh], setParams] = useState(() => ["", false]);
+  const [response, setResponse] = useState([undefined, new NS("INIT")]);
 
   const refresh = useCallback(() => setParams([uuidv4(), true]), []);
 
@@ -56,7 +51,7 @@ export function useFetch(url, opts) {
 
     const abortctrl = new AbortController();
 
-    setResponse(([, s]) => [undefined, s.clone('LOADING')]);
+    setResponse(([, s]) => [undefined, s.clone("LOADING")]);
     const startTime = performance.now();
 
     // recursive merge might be better solution
@@ -66,70 +61,50 @@ export function useFetch(url, opts) {
       headers: {
         ...defaultFetchOptions.headers,
         ...opts?.headers,
-        ...(fresh && {'X-Clear-Cache': true}),
-        'X-Request-ID': rId,
+        ...(fresh && { "X-Clear-Cache": true }),
+        "X-Request-ID": rId,
       },
     };
 
-    if (finalopts.headers['Content-Type'] === null) {
-      delete finalopts.headers['Content-Type'];
+    if (finalopts.headers["Content-Type"] === null) {
+      delete finalopts.headers["Content-Type"];
     }
     fetch(url, finalopts)
-        .then(async (res) => {
-          if (abortctrl.signal.aborted) return;
+      .then(async (res) => {
+        if (abortctrl.signal.aborted) return;
 
-          const responseTime = performance.now() - startTime;
-          const cached = !!res.headers.get('X-Browser-Cache');
-          let body;
+        const responseTime = performance.now() - startTime;
+        const cached = !!res.headers.get("X-Browser-Cache");
+        let body;
 
-          try {
-            body = await res.json();
-          } catch (e) {
-            const message = 'Invalid JSON response from API';
-            console.error(`${cr}API Error:${cr}${tab}URL: ${url}${cr}${
-                tab}Msg: ${message}${cr}${tab}Code: ${res.status}`);
-            setResponse(
-                ([, s]) =>
-                    [undefined,
-                     s.clone(
-                         'ERROR', '', res.status, responseTime, rId, cached)]);
-            return;
-          }
+        try {
+          body = await res.json();
+        } catch (e) {
+          const message = "Invalid JSON response from API";
+          console.error(`${cr}API Error:${cr}${tab}URL: ${url}${cr}${tab}Msg: ${message}${cr}${tab}Code: ${res.status}`);
+          setResponse(([, s]) => [undefined, s.clone("ERROR", "", res.status, responseTime, rId, cached)]);
+          return;
+        }
 
-          if (res.status >= 400) {
-            const errorType = body.error || '';
-            const isInternalError =
-                !errorType || errorType === 'Internal Server Error';
-            const message = !isInternalError ? body.message || '' : '';
-            setResponse(
-                ([, s]) =>
-                    [undefined,
-                     s.clone(
-                         'ERROR', message, res.status, responseTime, rId,
-                         cached, false)]);
-            return;
-          }
+        if (res.status >= 400) {
+          const errorType = body.error || "";
+          const isInternalError = !errorType || errorType === "Internal Server Error";
+          const message = !isInternalError ? body.message || "" : "";
+          setResponse(([, s]) => [undefined, s.clone("ERROR", message, res.status, responseTime, rId, cached, false)]);
+          return;
+        }
 
-          const dataType = getType(body);
-          const hasData = dataType !== 'Null' &&
-              (dataType === 'Array' ? body.length > 0 : true);
+        const dataType = getType(body);
+        const hasData = dataType !== "Null" && (dataType === "Array" ? body.length > 0 : true);
 
-          setResponse(
-              ([, s]) =>
-                  [body,
-                   s.clone(
-                       'SUCCESS', '', res.status, responseTime, rId, cached,
-                       hasData)]);
-        })
-        .catch((err) => {
-          if (abortctrl.signal.aborted) return;
-          const responseTime = performance.now() - startTime;
-          console.error(`${cr}API Error:${cr}${tab}URL: ${url}${cr}${tab}Msg: ${
-              err.message}${cr}${tab}Code: 0`);
-          setResponse(
-              ([,
-                s]) => [undefined, s.clone('ERROR', '', 0, responseTime, rId)]);
-        });
+        setResponse(([, s]) => [body, s.clone("SUCCESS", "", res.status, responseTime, rId, cached, hasData)]);
+      })
+      .catch((err) => {
+        if (abortctrl.signal.aborted) return;
+        const responseTime = performance.now() - startTime;
+        console.error(`${cr}API Error:${cr}${tab}URL: ${url}${cr}${tab}Msg: ${err.message}${cr}${tab}Code: 0`);
+        setResponse(([, s]) => [undefined, s.clone("ERROR", "", 0, responseTime, rId)]);
+      });
 
     return () => abortctrl.abort();
   }, [fresh, opts, rId, url]);
@@ -137,23 +112,21 @@ export function useFetch(url, opts) {
   return [response[0], response[1], refresh];
 }
 
-const API_BASE_URL =
-    process.env.REACT_APP_API_BASE_URL || window.location.origin;
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || window.location.origin;
 export default function useAPI(urlpath, extraOptions) {
   const [user, , logout] = useContext(AuthContext);
   const url = urlpath && new URL(urlpath, API_BASE_URL).toString();
 
   const options = useMemo(
-      () => ({
-        ...extraOptions,
-        headers: {
-          Authorization: `Bearer ${user?.token || ''}`,
-          ...extraOptions?.headers,
-        },
-      }),
-      [
-        extraOptions, user
-      ]);  // Don't add `user` . Adding will cause re-render when
+    () => ({
+      ...extraOptions,
+      headers: {
+        Authorization: `Bearer ${user?.token || ""}`,
+        ...extraOptions?.headers,
+      },
+    }),
+    [extraOptions, user]
+  ); // Don't add `user` . Adding will cause re-render when
   // token change (ex: /login api)
 
   const [data, status, refresh] = useFetch(url, options);
@@ -163,7 +136,7 @@ export default function useAPI(urlpath, extraOptions) {
       logout();
     }
     if (status.isError && !status.errorCaught && status.statusCode >= 400) {
-      message.error('Oops! Something went wrong.', 3);
+      message.error("Oops! Something went wrong.", 3);
     }
   }, [status, logout]);
 
@@ -173,49 +146,45 @@ export default function useAPI(urlpath, extraOptions) {
 export const mergeStatuses = (...statuses) => {
   const hasData = statuses.some((s) => s.hasData);
   const statusWithError = statuses.find((s) => s.isError);
-  const message = statusWithError ? statusWithError.message : '';
+  const message = statusWithError ? statusWithError.message : "";
   const statusCode = statusWithError ? statusWithError.statusCode : 0;
 
-  const status = statuses.every((s) => s.isSuccess) ?
-      'SUCCESS'  // [SUCCESS, SUCCESS, SUCCESS]
-      :
-      statusWithError ?
-      'ERROR'  // [SUCCESS, ERROR] or [LOADING, ERROR]
-      :
-      statuses.find((s) => s.isLoading) ?
-      'LOADING'  // [SUCCESS, LOADING] or [LOADING, INIT] or [LOADING, LOADING]
-      :
-      statuses.find((s) => s.isSuccess) ? 'LOADING'  // [SUCCESS, INIT]
-                                          :
-                                          'INIT';  // [INIT, INIT]
+  const status = statuses.every((s) => s.isSuccess)
+    ? "SUCCESS" // [SUCCESS, SUCCESS, SUCCESS]
+    : statusWithError
+    ? "ERROR" // [SUCCESS, ERROR] or [LOADING, ERROR]
+    : statuses.find((s) => s.isLoading)
+    ? "LOADING" // [SUCCESS, LOADING] or [LOADING, INIT] or [LOADING, LOADING]
+    : statuses.find((s) => s.isSuccess)
+    ? "LOADING" // [SUCCESS, INIT]
+    : "INIT"; // [INIT, INIT]
 
-  return new NS(status, message, statusCode, 0, '', false, hasData);
+  return new NS(status, message, statusCode, 0, "", false, hasData);
 };
 
 export function useInsurerIdAndNames() {
   const queryParams = new URLSearchParams([
-    ['fields', ['id', 'name']],
-    ['sortBy', 'name'],
+    ["fields", ["id", "name"]],
+    ["sortBy", "name"],
   ]);
   const [insurers = [], status] = useAPI(`/api/v1/insurers?${queryParams}`);
   return [insurers, status];
 }
 export function useBrands() {
   const queryParams = new URLSearchParams([
-    ['fields', ['id', 'name']],
-    ['sortBy', 'name'],
+    ["fields", ["id", "name"]],
+    ["sortBy", "name"],
   ]);
-  const [patients = [], status, refresh] =
-      useAPI(`/api/v1/brands?${queryParams}`);
+  const [patients = [], status, refresh] = useAPI(`/api/v1/brands?${queryParams}`);
   return [patients, status, refresh];
 }
 export function useCategories() {
   const queryParams = new URLSearchParams([
-    ['fields', ['id', 'name']],
-    ['sortBy', 'name'],
+    ["fields", ["id", "name"]],
+    ["sortBy", "name"],
   ]);
-  const [physicians = [], status] = useAPI(`/api/v1/categories?${queryParams}`);
-  return [physicians, status];
+  const [physicians = [], status, refresh] = useAPI(`/api/v1/categories?${queryParams}`);
+  return [physicians, status, refresh];
 }
 
 export function useBranches() {
@@ -224,17 +193,17 @@ export function useBranches() {
 }
 
 export function useOrganizations() {
-  const [organizations = [], status] = useAPI('/api/v1/organizations');
+  const [organizations = [], status] = useAPI("/api/v1/organizations");
   return [organizations, status];
 }
 
 export function useOrderStatuses() {
-  const [statuses = [], status] = useAPI('/api/v1/orderstatuses');
+  const [statuses = [], status] = useAPI("/api/v1/orderstatuses");
   return [statuses, status];
 }
 
 export function useEquiments() {
-  const [equiments = [], status] = useAPI('/api/v1/equipments');
+  const [equiments = [], status] = useAPI("/api/v1/equipments");
   return [equiments, status];
 }
 
@@ -245,14 +214,12 @@ export function useSalesUsers() {
 
 export function useProduct(id) {
   const [payload, setPayload] = useState(undefined);
-  const [product, status, refresh] =
-      useAPI(id ? `/api/v1/products/${id}` : undefined);
+  const [product, status, refresh] = useAPI(id ? `/api/v1/products/${id}` : undefined);
 
   const saveOpts = useMemo(() => {
     if (!payload) return [undefined];
-    const [urlpath, method] =
-        id ? [`/api/v1/products/${id}`, 'PUT'] : ['/api/v1/products', 'POST'];
-    return [urlpath, {method, body: JSON.stringify(payload)}];
+    const [urlpath, method] = id ? [`/api/v1/products/${id}`, "PUT"] : ["/api/v1/products", "POST"];
+    return [urlpath, { method, body: JSON.stringify(payload) }];
   }, [payload, id]);
   const [, saveStatus] = useAPI(...saveOpts);
 
@@ -261,14 +228,12 @@ export function useProduct(id) {
 
 export function useCustomer(id) {
   const [payload, setPayload] = useState(undefined);
-  const [product, status, refresh] =
-      useAPI(id ? `/api/v1/customers/${id}` : undefined);
+  const [product, status, refresh] = useAPI(id ? `/api/v1/customers/${id}` : undefined);
 
   const saveOpts = useMemo(() => {
     if (!payload) return [undefined];
-    const [urlpath, method] =
-        id ? [`/api/v1/customers/${id}`, 'PUT'] : ['/api/v1/customers', 'POST'];
-    return [urlpath, {method, body: JSON.stringify(payload)}];
+    const [urlpath, method] = id ? [`/api/v1/customers/${id}`, "PUT"] : ["/api/v1/customers", "POST"];
+    return [urlpath, { method, body: JSON.stringify(payload) }];
   }, [payload, id]);
   const [, saveStatus] = useAPI(...saveOpts);
 
@@ -277,15 +242,13 @@ export function useCustomer(id) {
 
 export function useOrder(id) {
   const [payload, setPayload] = useState(undefined);
-  const [order, status, refresh] =
-      useAPI(id ? `/api/v1/orders/${id}` : undefined);
+  const [order, status, refresh] = useAPI(id ? `/api/v1/orders/${id}` : undefined);
 
   const saveOpts = useMemo(() => {
-    console.log('p :>> ', payload, id);
+    console.log("p :>> ", payload, id);
     if (!payload) return [undefined];
-    const [urlpath, method] =
-        id ? [`/api/v1/orders/${id}`, 'PUT'] : ['/api/v1/orders', 'POST'];
-    return [urlpath, {method, body: JSON.stringify(payload)}];
+    const [urlpath, method] = id ? [`/api/v1/orders/${id}`, "PUT"] : ["/api/v1/orders", "POST"];
+    return [urlpath, { method, body: JSON.stringify(payload) }];
   }, [payload, id]);
   const [, saveStatus] = useAPI(...saveOpts);
 
@@ -293,14 +256,14 @@ export function useOrder(id) {
 }
 
 export function useCustomers() {
-  const [customers = [], status, refresh] = useAPI('/api/v1/customers');
+  const [customers = [], status, refresh] = useAPI("/api/v1/customers");
   return [customers, status, refresh];
 }
 export function useProducts() {
-  const [products = [], status, refresh] = useAPI('/api/v1/products');
+  const [products = [], status, refresh] = useAPI("/api/v1/products");
   return [products, status, refresh];
 }
 export function useOrders() {
-  const [orders = [], status, refresh] = useAPI('/api/v1/orders');
+  const [orders = [], status, refresh] = useAPI("/api/v1/orders");
   return [orders, status, refresh];
 }
